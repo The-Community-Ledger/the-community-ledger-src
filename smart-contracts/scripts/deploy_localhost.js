8a1956027b912573fffd3c32dbc0296d85c665ce
Johan Medrano <johmedr0@mit.edu>
2025-04-04 19:57:44 -0400

    Add simple JCR token - seems to deploy ok

--- smart-contracts/scripts/deploy_localhost.js
+++ smart-contracts/scripts/deploy_localhost.js
@@ -3,6 +3,12 @@ const { ethers } = require("hardhat");
 async function main() {
   const wallContract = await ethers.deployContract("Wall");
   await wallContract.waitForDeployment();
+
+  const initialSupply = ethers.parseEther("1000000000"); // 1,000,000,000 tokens
+  const JournalCredit = await ethers.getContractFactory("JournalCredit");
+  const journalCredit = await JournalCredit.deploy(initialSupply);
+  await journalCredit.waitForDeployment();
+  console.log("JournalCredit deployed to:", journalCredit.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
